92a5b7950ebc714bc398a80b72545d891f0f6b88
RyuseiNomi <kanontonto1010@gmail.com>
2023-10-24 10:55:18 +0900

    create a map view

--- CarNavigationWithKAITO/ContentView.swift
+++ CarNavigationWithKAITO/ContentView.swift
@@ -6,16 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @ObservedObject  var manager = LocationManager()
+        
+    // ユーザートラッキングモードを追従モードにするための変数を定義
+    @State  var trackingMode = MapUserTrackingMode.follow
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(coordinateRegion: $manager.region,
+            showsUserLocation: true, // マップ上にユーザーの場所を表示するオプションをBool値で指定
+            userTrackingMode: $trackingMode) // マップがユーザーの位置情報更新にどのように応答するかを決定
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
