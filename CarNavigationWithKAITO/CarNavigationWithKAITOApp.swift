b556c6cc8d3fad3e47ce53768828f41efed1187f
RyuseiNomi <kanontonto1010@gmail.com>
2023-10-24 10:05:57 +0900

    Initial Commit

---
+++ CarNavigationWithKAITO/CarNavigationWithKAITOApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarNavigationWithKAITOApp.swift
+//  CarNavigationWithKAITO
+//
+//  Created by とんと on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CarNavigationWithKAITOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
